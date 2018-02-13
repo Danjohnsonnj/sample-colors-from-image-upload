68107383ef7964642782cc5be4518f843201217a
Daniel Johnson <danjohnsonnj@gmail.com>
2018-02-12 21:17:13 -0500

    Hooking up controls

--- src/site.js
+++ src/site.js
@@ -13,10 +13,22 @@ document.addEventListener('DOMContentLoaded', () => {
   inputSamples.value = DEFAULTS.samples
   inputSampleSize.value = DEFAULTS.sampleSize
 
+  inputSamples.nextElementSibling.innerHTML = inputSamples.value
+  inputSampleSize.nextElementSibling.innerHTML = inputSampleSize.value
+
+  inputSamples.addEventListener('change', evt => {
+    evt.currentTarget.nextElementSibling.innerHTML = evt.currentTarget.value
+  })
+
+  inputSampleSize.addEventListener('change', evt => {
+    evt.currentTarget.nextElementSibling.innerHTML = evt.currentTarget.value
+  })
+
   buttonSet.addEventListener('click', evt => {
     evt.preventDefault()
-    window.previewer.samples = inputSampleSize.value
+    window.previewer.samples = inputSamples.value
     window.previewer.sampleSize = inputSampleSize.value
+    window.previewer.updateImageDisplay()
   })
 
 })
