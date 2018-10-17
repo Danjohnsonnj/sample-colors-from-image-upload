f3ebb45cd97179643b4d95c2982542645152c3e4
Dan Johnson <danjohnsonnj@gmail.com>
2018-10-16 20:04:21 -0400

    Separates logic for colors, gradient string and swatches

--- src/ImagePreview.js
+++ src/ImagePreview.js
@@ -59,19 +59,23 @@ class ImagePreview {
     image = document.createElement('img')
     image.addEventListener('load', () => {
       this.sampler.drawImageToCanvas(image)
-      this.showSwatches(image)
+      const colors = this.getColors()
+      const gradientCoords = this.getGradientCoords()
+      const gradientString = this.getGradientString({ colors, gradientCoords, })
+      const swatches = this.buildSwatches({ colors, gradientString, })
+      console.log(colors, gradientString, swatches)
+      this.swatchWrapper.appendChild(swatches)
     })
     image.src = window.URL.createObjectURL(curFile)
 
     this.preview.appendChild(image)
   }
 
-  showSwatches(image) {
+  getColors() {
     const sampleSize = this._samples
     const width = this.sampler.canvas.width - 1
     const height = this.sampler.canvas.height - 1
     const colors = []
-    const gradientCoords = []
     const length = sampleSize - 1
     for (let h = 0; h <= length; h++) {
       for (let w = 0; w <= length; w++) {
@@ -79,14 +83,53 @@ class ImagePreview {
           Math.floor(width * w / length),
           Math.floor(height * h / length)
         ))
-        gradientCoords.push(`${(w * 100 / sampleSize) + (100 / sampleSize / 2)}%
-          ${(h * 100 / sampleSize) + (100 / sampleSize / 2)}%`)
-        // console.log(gradientCoords[gradientCoords.length - 1])
       }
     }
 
+    return colors
+  }
+
+  getGradientCoords() {
+    const sampleSize = this._samples
+    const length = sampleSize - 1
+    const gradientCoords = []
+    for (let h = 0; h <= length; h++) {
+      for (let w = 0; w <= length; w++) {
+        gradientCoords.push(`${(w * 100 / sampleSize) + (100 / sampleSize / 2)}%
+        ${(h * 100 / sampleSize) + (100 / sampleSize / 2)}%`)
+      }
+    }
+    return gradientCoords
+  }
+
+  getGradientString({ colors, gradientCoords, }) {
     let gradientArray = []
+    const sampleSize = this._samples
     const vanishingPoint = Math.floor(100 / sampleSize) * 1.2
+    colors.forEach((c, x) => {
+      gradientArray.push(`radial-gradient(circle at ${gradientCoords[x]}, ${colors[x]} 0%,
+        ${this.sampler.chroma(colors[x]).alpha(0).css()} ${vanishingPoint}%)`)
+    })
+
+    const gMid = Math.floor(gradientArray.length / 2)
+    const gRange = Math.floor(Math.sqrt(gradientArray.length) / 2)
+    const gradientString = gradientArray.reduce((acc, i, d, a) => {
+      if (Math.abs(d - gMid) < gRange) {
+        acc.unshift(a.slice(d, d + 1)[0])
+      } else {
+        acc.push(i)
+      }
+      return acc
+    }, []).join()
+
+    return gradientString
+  }
+
+  buildSwatches({ colors, gradientString, }) {
+    this.swatchWrapper.style.backgroundImage = gradientString
+    this.swatchWrapper.style.backgroundColor = this.sampler.getAverageOfColors(colors)
+
+    const sampleSize = this._samples
     const imageRatio = this.sampler.getImageRatio()
     const wrap = document.createElement('div')
     colors.forEach((c, x) => {
@@ -96,25 +139,9 @@ class ImagePreview {
       tile.style.flexBasis = `${100 / sampleSize}%`
       tile.style.paddingBottom = `calc(${100 / sampleSize}% * ${imageRatio})`
       wrap.appendChild(tile)
-
-      gradientArray.push(`radial-gradient(circle at ${gradientCoords[x]}, ${colors[x]} 0%,
-        ${this.sampler.chroma(colors[x]).alpha(0).css()} ${vanishingPoint}%)`)
     })
 
-    const gMid = Math.floor(gradientArray.length / 2)
-    const gRange = Math.floor(Math.sqrt(gradientArray.length) / 2)
-    const acc = []
-    gradientArray.forEach((i, d, a) => {
-      if (Math.abs(d - gMid) < gRange) {
-        acc.unshift(a.slice(d, d + 1)[0])
-      } else {
-        acc.push(i)
-      }
-    })
-    const gradientString = acc.join()
-    this.swatchWrapper.style.backgroundImage = gradientString
-    this.swatchWrapper.style.backgroundColor = this.sampler.getAverageOfColors(colors)
-    this.swatchWrapper.appendChild(wrap)
+    return wrap
   }
 
   set samples(num) {
